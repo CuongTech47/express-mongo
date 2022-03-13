4becbb789f46992bb1f9fcbc2344b7a5ac5eeea4
cuongtech47 <cuongtech47@gmail.com>
2022-03-13 22:41:00 +0700

    add .env

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,13 @@ const categoryRouter = require('./category')
 const brandRouter = require('./brand')
 const productRouter = require('./prouduct')
 function route(app) {
+  
   app.use('/api/v1/product',categoryRouter)
   app.use('/api/v1/brand',brandRouter)
   app.use('/api/v1/category',categoryRouter)
+  app.use('/',(req,res)=>{
+    res.send("hello api")
+  })
   
   /* app.use("/auth",authRouter)
   app.use('/api/v1/user',userRouter) */
